68e2a14b46c3942def6f537d5f4c9160575cc525
mattbahr <mattbahr1992@gmail.com>
2014-07-20 16:57:39 -0400

    Edited Card Czar Select Class
    Added logic to determine how many of the white card relative layouts
    need to be displayed; incomplete as of now but can't do more until I
    have more of the back end set up

--- app/src/main/java/com/cardsagainsthumanity/app/CardCzarSelect.java
+++ app/src/main/java/com/cardsagainsthumanity/app/CardCzarSelect.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /**
  * There are 24 card images in the Linear Layout; not all of them will have a submitted white card associated with them
@@ -37,6 +41,443 @@ public class CardCzarSelect extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void displaySubmittedCardText(){
+        int i = 0;
+        int k = 0;
+        int j = 0;
+
+        TextView temp = (TextView) findViewById(R.id.txtSubWhiteCard1);
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.RLSubCard1);
+
+        if(j < Game.numPlayers*Game.getCurrentBlackCard().getNumPrompts()){
+            temp.setText(Game.submittedCards.get(i).get(k).getText());
+            k++;
+
+            if(k == Game.getCurrentBlackCard().getNumPrompts()){
+                k = 0;
+                i++;
+            }
+        }
+
+        else{
+            rl.setVisibility(View.GONE);
+        }
+
+        j++;
+        temp = (TextView) findViewById(R.id.txtSubWhiteCard2);
+        rl = (RelativeLayout) findViewById(R.id.RLSubCard2);
+
+        if(j < Game.numPlayers*Game.getCurrentBlackCard().getNumPrompts()){
+            temp.setText(Game.submittedCards.get(i).get(k).getText());
+            k++;
+
+            if(k == Game.getCurrentBlackCard().getNumPrompts()){
+                k = 0;
+                i++;
+            }
+        }
+
+        else{
+            rl.setVisibility(View.GONE);
+        }
+
+        j++;
+        temp = (TextView) findViewById(R.id.txtSubWhiteCard3);
+        rl = (RelativeLayout) findViewById(R.id.RLSubCard3);
+
+        if(j < Game.numPlayers*Game.getCurrentBlackCard().getNumPrompts()){
+            temp.setText(Game.submittedCards.get(i).get(k).getText());
+            k++;
+
+            if(k == Game.getCurrentBlackCard().getNumPrompts()){
+                k = 0;
+                i++;
+            }
+        }
+
+        else{
+            rl.setVisibility(View.GONE);
+        }
+
+        j++;
+        temp = (TextView) findViewById(R.id.txtSubWhiteCard4);
+        rl = (RelativeLayout) findViewById(R.id.RLSubCard4);
+
+        if(j < Game.numPlayers*Game.getCurrentBlackCard().getNumPrompts()){
+            temp.setText(Game.submittedCards.get(i).get(k).getText());
+            k++;
+
+            if(k == Game.getCurrentBlackCard().getNumPrompts()){
+                k = 0;
+                i++;
+            }
+        }
+
+        else{
+            rl.setVisibility(View.GONE);
+        }
+
+        j++;
+        temp = (TextView) findViewById(R.id.txtSubWhiteCard5);
+        rl = (RelativeLayout) findViewById(R.id.RLSubCard5);
+
+        if(j < Game.numPlayers*Game.getCurrentBlackCard().getNumPrompts()){
+            temp.setText(Game.submittedCards.get(i).get(k).getText());
+            k++;
+
+            if(k == Game.getCurrentBlackCard().getNumPrompts()){
+                k = 0;
+                i++;
+            }
+        }
+
+        else{
+            rl.setVisibility(View.GONE);
+        }
+
+        j++;
+        temp = (TextView) findViewById(R.id.txtSubWhiteCard6);
+        rl = (RelativeLayout) findViewById(R.id.RLSubCard6);
+
+        if(j < Game.numPlayers*Game.getCurrentBlackCard().getNumPrompts()){
+            temp.setText(Game.submittedCards.get(i).get(k).getText());
+            k++;
+
+            if(k == Game.getCurrentBlackCard().getNumPrompts()){
+                k = 0;
+                i++;
+            }
+        }
+
+        else{
+            rl.setVisibility(View.GONE);
+        }
+
+        j++;
+        temp = (TextView) findViewById(R.id.txtSubWhiteCard7);
+        rl = (RelativeLayout) findViewById(R.id.RLSubCard7);
+
+        if(j < Game.numPlayers*Game.getCurrentBlackCard().getNumPrompts()){
+            temp.setText(Game.submittedCards.get(i).get(k).getText());
+            k++;
+
+            if(k == Game.getCurrentBlackCard().getNumPrompts()){
+                k = 0;
+                i++;
+            }
+        }
+
+        else{
+            rl.setVisibility(View.GONE);
+        }
+
+        j++;
+        temp = (TextView) findViewById(R.id.txtSubWhiteCard8);
+        rl = (RelativeLayout) findViewById(R.id.RLSubCard8);
+
+        if(j < Game.numPlayers*Game.getCurrentBlackCard().getNumPrompts()){
+            temp.setText(Game.submittedCards.get(i).get(k).getText());
+            k++;
+
+            if(k == Game.getCurrentBlackCard().getNumPrompts()){
+                k = 0;
+                i++;
+            }
+        }
+
+        else{
+            rl.setVisibility(View.GONE);
+        }
+
+        j++;
+        temp = (TextView) findViewById(R.id.txtSubWhiteCard9);
+        rl = (RelativeLayout) findViewById(R.id.RLSubCard9);
+
+        if(j < Game.numPlayers*Game.getCurrentBlackCard().getNumPrompts()){
+            temp.setText(Game.submittedCards.get(i).get(k).getText());
+            k++;
+
+            if(k == Game.getCurrentBlackCard().getNumPrompts()){
+                k = 0;
+                i++;
+            }
+        }
+
+        else{
+            rl.setVisibility(View.GONE);
+        }
+
+        j++;
+        temp = (TextView) findViewById(R.id.txtSubWhiteCard10);
+        rl = (RelativeLayout) findViewById(R.id.RLSubCard10);
+
+        if(j < Game.numPlayers*Game.getCurrentBlackCard().getNumPrompts()){
+            temp.setText(Game.submittedCards.get(i).get(k).getText());
+            k++;
+
+            if(k == Game.getCurrentBlackCard().getNumPrompts()){
+                k = 0;
+                i++;
+            }
+        }
+
+        else{
+            rl.setVisibility(View.GONE);
+        }
+
+        j++;
+        temp = (TextView) findViewById(R.id.txtSubWhiteCard11);
+        rl = (RelativeLayout) findViewById(R.id.RLSubCard11);
+
+        if(j < Game.numPlayers*Game.getCurrentBlackCard().getNumPrompts()){
+            temp.setText(Game.submittedCards.get(i).get(k).getText());
+            k++;
+
+            if(k == Game.getCurrentBlackCard().getNumPrompts()){
+                k = 0;
+                i++;
+            }
+        }
+
+        else{
+            rl.setVisibility(View.GONE);
+        }
+
+        j++;
+        temp = (TextView) findViewById(R.id.txtSubWhiteCard12);
+        rl = (RelativeLayout) findViewById(R.id.RLSubCard12);
+
+        if(j < Game.numPlayers*Game.getCurrentBlackCard().getNumPrompts()){
+            temp.setText(Game.submittedCards.get(i).get(k).getText());
+            k++;
+
+            if(k == Game.getCurrentBlackCard().getNumPrompts()){
+                k = 0;
+                i++;
+            }
+        }
+
+        else{
+            rl.setVisibility(View.GONE);
+        }
+
+        j++;
+        temp = (TextView) findViewById(R.id.txtSubWhiteCard13);
+        rl = (RelativeLayout) findViewById(R.id.RLSubCard13);
+
+        if(j < Game.numPlayers*Game.getCurrentBlackCard().getNumPrompts()){
+            temp.setText(Game.submittedCards.get(i).get(k).getText());
+            k++;
+
+            if(k == Game.getCurrentBlackCard().getNumPrompts()){
+                k = 0;
+                i++;
+            }
+        }
+
+        else{
+            rl.setVisibility(View.GONE);
+        }
+
+        j++;
+        temp = (TextView) findViewById(R.id.txtSubWhiteCard14);
+        rl = (RelativeLayout) findViewById(R.id.RLSubCard14);
+
+        if(j < Game.numPlayers*Game.getCurrentBlackCard().getNumPrompts()){
+            temp.setText(Game.submittedCards.get(i).get(k).getText());
+            k++;
+
+            if(k == Game.getCurrentBlackCard().getNumPrompts()){
+                k = 0;
+                i++;
+            }
+        }
+
+        else{
+            rl.setVisibility(View.GONE);
+        }
+
+        j++;
+        temp = (TextView) findViewById(R.id.txtSubWhiteCard15);
+        rl = (RelativeLayout) findViewById(R.id.RLSubCard15);
+
+        if(j < Game.numPlayers*Game.getCurrentBlackCard().getNumPrompts()){
+            temp.setText(Game.submittedCards.get(i).get(k).getText());
+            k++;
+
+            if(k == Game.getCurrentBlackCard().getNumPrompts()){
+                k = 0;
+                i++;
+            }
+        }
+
+        else{
+            rl.setVisibility(View.GONE);
+        }
+
+        j++;
+        temp = (TextView) findViewById(R.id.txtSubWhiteCard16);
+        rl = (RelativeLayout) findViewById(R.id.RLSubCard16);
+
+        if(j < Game.numPlayers*Game.getCurrentBlackCard().getNumPrompts()){
+            temp.setText(Game.submittedCards.get(i).get(k).getText());
+            k++;
+
+            if(k == Game.getCurrentBlackCard().getNumPrompts()){
+                k = 0;
+                i++;
+            }
+        }
+
+        else{
+            rl.setVisibility(View.GONE);
+        }
+
+        j++;
+        temp = (TextView) findViewById(R.id.txtSubWhiteCard17);
+        rl = (RelativeLayout) findViewById(R.id.RLSubCard17);
+
+        if(j < Game.numPlayers*Game.getCurrentBlackCard().getNumPrompts()){
+            temp.setText(Game.submittedCards.get(i).get(k).getText());
+            k++;
+
+            if(k == Game.getCurrentBlackCard().getNumPrompts()){
+                k = 0;
+                i++;
+            }
+        }
+
+        else{
+            rl.setVisibility(View.GONE);
+        }
+
+        j++;
+        temp = (TextView) findViewById(R.id.txtSubWhiteCard18);
+        rl = (RelativeLayout) findViewById(R.id.RLSubCard18);
+
+        if(j < Game.numPlayers*Game.getCurrentBlackCard().getNumPrompts()){
+            temp.setText(Game.submittedCards.get(i).get(k).getText());
+            k++;
+
+            if(k == Game.getCurrentBlackCard().getNumPrompts()){
+                k = 0;
+                i++;
+            }
+        }
+
+        else{
+            rl.setVisibility(View.GONE);
+        }
+
+        j++;
+        temp = (TextView) findViewById(R.id.txtSubWhiteCard19);
+        rl = (RelativeLayout) findViewById(R.id.RLSubCard19);
+
+        if(j < Game.numPlayers*Game.getCurrentBlackCard().getNumPrompts()){
+            temp.setText(Game.submittedCards.get(i).get(k).getText());
+            k++;
+
+            if(k == Game.getCurrentBlackCard().getNumPrompts()){
+                k = 0;
+                i++;
+            }
+        }
+
+        else{
+            rl.setVisibility(View.GONE);
+        }
+
+        j++;
+        temp = (TextView) findViewById(R.id.txtSubWhiteCard20);
+        rl = (RelativeLayout) findViewById(R.id.RLSubCard20);
+
+        if(j < Game.numPlayers*Game.getCurrentBlackCard().getNumPrompts()){
+            temp.setText(Game.submittedCards.get(i).get(k).getText());
+            k++;
+
+            if(k == Game.getCurrentBlackCard().getNumPrompts()){
+                k = 0;
+                i++;
+            }
+        }
+
+        else{
+            rl.setVisibility(View.GONE);
+        }
+
+        j++;
+        temp = (TextView) findViewById(R.id.txtSubWhiteCard21);
+        rl = (RelativeLayout) findViewById(R.id.RLSubCard21);
+
+        if(j < Game.numPlayers*Game.getCurrentBlackCard().getNumPrompts()){
+            temp.setText(Game.submittedCards.get(i).get(k).getText());
+            k++;
+
+            if(k == Game.getCurrentBlackCard().getNumPrompts()){
+                k = 0;
+                i++;
+            }
+        }
+
+        else{
+            rl.setVisibility(View.GONE);
+        }
+
+        j++;
+        temp = (TextView) findViewById(R.id.txtSubWhiteCard22);
+        rl = (RelativeLayout) findViewById(R.id.RLSubCard22);
+
+        if(j < Game.numPlayers*Game.getCurrentBlackCard().getNumPrompts()){
+            temp.setText(Game.submittedCards.get(i).get(k).getText());
+            k++;
+
+            if(k == Game.getCurrentBlackCard().getNumPrompts()){
+                k = 0;
+                i++;
+            }
+        }
+
+        else{
+            rl.setVisibility(View.GONE);
+        }
+
+        j++;
+        temp = (TextView) findViewById(R.id.txtSubWhiteCard23);
+        rl = (RelativeLayout) findViewById(R.id.RLSubCard23);
+
+        if(j < Game.numPlayers*Game.getCurrentBlackCard().getNumPrompts()){
+            temp.setText(Game.submittedCards.get(i).get(k).getText());
+            k++;
+
+            if(k == Game.getCurrentBlackCard().getNumPrompts()){
+                k = 0;
+                i++;
+            }
+        }
+
+        else{
+            rl.setVisibility(View.GONE);
+        }
+
+        j++;
+        temp = (TextView) findViewById(R.id.txtSubWhiteCard24);
+        rl = (RelativeLayout) findViewById(R.id.RLSubCard24);
+
+        if(j < Game.numPlayers*Game.getCurrentBlackCard().getNumPrompts()){
+            temp.setText(Game.submittedCards.get(i).get(k).getText());
+            k++;
+
+            if(k == Game.getCurrentBlackCard().getNumPrompts()){
+                k = 0;
+                i++;
+            }
+        }
+
+        else{
+            rl.setVisibility(View.GONE);
+        }
     }
 
 }
